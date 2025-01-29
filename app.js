64d8ce4652d79fde7fdcb0956d51967e80d4bc1a
indumatic <electrico@indumatic.net>
2025-01-29 13:29:32 +0100

    added test script

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const blogsRouter = require('./controllers/blogs')
+mongoose.set('strictQuery', false)
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
