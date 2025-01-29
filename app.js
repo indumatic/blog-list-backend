441208074dde6a77e08edce00703ff7054396217
indumatic <electrico@indumatic.net>
2025-01-29 12:57:18 +0100

    moved blog routers into its own controller

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const config = require('./utils/config')
-const Blog = require('./models/blog')
 
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
